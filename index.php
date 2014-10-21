1c289e909fe17cefbee91240e2c46bda3d3b6e8b
Bargetor <bargetor@gmail.com>
2014-10-21 12:07:17 +0800

    index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" >
+        <meta http-equiv="Content-Language" content="zh">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" type="text/css" href="css/font.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+
+    <body>
+        <div>
+            <p>BesideBamboo</p>
+            <p>Pure & Natural</p>
+        </div>
+    </body>
+</html>
